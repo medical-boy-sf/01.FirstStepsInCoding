e8c167a5672b41ab7cd4979aecab415f59d52d49
Someone Else <medical_boy_sf@yahoo.com>
2018-04-16 09:22:54 +0300

    All tasks

---
+++ 06.SquareOfStars/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _06.SquareOfStars
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            if (n >= 3 && n <= 100)
+            {
+                Console.WriteLine(new String('*', n));
+
+                for (int i = 1; i <= n - 2; i++)
+                {
+                    Console.Write("*");
+                    Console.Write(new string(' ', n - 2));
+                    Console.WriteLine("*");
+                }
+
+                Console.WriteLine(new String('*', n));
+            }
+        }
+    }
+}
