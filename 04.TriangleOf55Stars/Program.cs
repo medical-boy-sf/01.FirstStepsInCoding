74ebbf443f151f069d493081e29d3f086cdd974e
Someone Else <medical_boy_sf@yahoo.com>
2018-04-16 09:10:12 +0300

    Tasks 01-04

---
+++ 04.TriangleOf55Stars/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _04.TriangleOf55Stars
+{
+    class Program
+    {
+        static void Main()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(new String('*', i));
+            }
+        }
+    }
+}
